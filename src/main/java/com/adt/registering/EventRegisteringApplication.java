8e9faffa97d07dc249a2073aedf4daa00decb16d
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 10:32:10 -0500

    init: initial commit

---
+++ src/main/java/com/adt/registering/EventRegisteringApplication.java
@@ -0,0 +1,13 @@
+package com.adt.registering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegisteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegisteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adt/registering/EventRegisteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adt.registering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegisteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
