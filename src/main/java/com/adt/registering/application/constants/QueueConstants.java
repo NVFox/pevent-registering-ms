1c3d8bd145ada7c9b7292cd18acbbd47e93763aa
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 14:55:35 -0500

    feat: add queue config for rabbit messaging broker

---
+++ src/main/java/com/adt/registering/application/constants/QueueConstants.java
@@ -0,0 +1,11 @@
+package com.adt.registering.application.constants;
+
+public class QueueConstants {
+    private QueueConstants() {}
+
+    public static final String NOTIFICATIONS_TOPIC_NAME = "notifications";
+
+    public static final String EVENTS_QUEUE_NAME = "events";
+
+    public static final String EVENTS_CHANNEL_NAME = "channel:events";
+}
