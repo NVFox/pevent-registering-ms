ca09efefbcbf9bd4cf66b73f2ec4a79fb2c311c8
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 15:49:10 -0500

    feat: add log service usage in create event command handler

--- src/main/java/com/adt/registering/application/commands/handlers/CreateEventCommandHandler.java
+++ src/main/java/com/adt/registering/application/commands/handlers/CreateEventCommandHandler.java
@@ -2,15 +2,19 @@ package com.adt.registering.application.commands.handlers;
 
 import com.adt.registering.application.commands.CreateEventCommand;
 import com.adt.registering.domain.entities.Event;
+import com.adt.registering.domain.entities.Log;
 import com.adt.registering.domain.services.EventService;
+import com.adt.registering.domain.services.LogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
 @RequiredArgsConstructor
 public class CreateEventCommandHandler {
     private final EventService eventService;
+    private final LogService logService;
 
     public Mono<Event> handle(CreateEventCommand command) {
         Event event = new Event();
@@ -18,6 +22,22 @@ public class CreateEventCommandHandler {
         event.setType(command.type());
         event.setDescription(command.description());
 
-        return eventService.create(event);
+        return logService.create(Log.info("** Starting event registration **"))
+                .then(eventService.create(event))
+                .publishOn(Schedulers.boundedElastic())
+                .doOnError(error ->
+                    log(Log.error("Error creating event: " + error.getMessage() +
+                            " - with command: " + command)))
+                .doOnSuccess(saved ->
+                    log(Log.info("Event created: " + saved.getId() +
+                            " - with command: " + command)))
+                .doFinally(signalType ->
+                        log(Log.info("** Event registration completed with signal " + signalType + " **")));
+    }
+
+    private void log(Log log) {
+        logService.create(log)
+                .retry(3)
+                .subscribe();
     }
 }
--- src/main/java/com/adt/registering/application/services/LogServiceImpl.java
+++ src/main/java/com/adt/registering/application/services/LogServiceImpl.java
@@ -5,6 +5,7 @@ import com.adt.registering.domain.repositories.LogRepository;
 import com.adt.registering.domain.repositories.filters.LogFilter;
 import com.adt.registering.domain.services.LogService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,15 @@ import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LogServiceImpl implements LogService {
     private final LogRepository logRepository;
 
     @Override
     public Mono<Log> create(Log log) {
-        return logRepository.save(log);
+        return logRepository.save(log)
+                .doOnSuccess(saved -> LogServiceImpl.log.info("Log created: {}", log))
+                .doOnError(error -> LogServiceImpl.log.error("Error creating log: {}", log, error));
     }
 
     @Override
