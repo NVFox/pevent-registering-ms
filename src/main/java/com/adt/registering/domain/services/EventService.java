776579e2d7702f35d92c51281ee20ba811e06e75
Andrés Téllez <davidroa8@hotmail.com>
2025-04-12 13:41:19 -0500

    fix: change services method signature

--- src/main/java/com/adt/registering/domain/services/EventService.java
+++ src/main/java/com/adt/registering/domain/services/EventService.java
@@ -2,10 +2,11 @@ package com.adt.registering.domain.services;
 
 import com.adt.registering.domain.entities.Event;
 import com.adt.registering.domain.repositories.filters.EventFilter;
-import reactor.core.publisher.Flux;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Mono;
 
 public interface EventService {
     Mono<Event> create(Event event);
-    Flux<Event> findBy(EventFilter filter);
+    Mono<Page<Event>> findBy(EventFilter filter, Pageable pageable);
 }
--- src/main/java/com/adt/registering/domain/services/LogService.java
+++ src/main/java/com/adt/registering/domain/services/LogService.java
@@ -2,10 +2,12 @@ package com.adt.registering.domain.services;
 
 import com.adt.registering.domain.entities.Log;
 import com.adt.registering.domain.repositories.filters.LogFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface LogService {
     Mono<Log> create(Log log);
-    Flux<Log> findBy(LogFilter filter);
+    Mono<Page<Log>> findBy(LogFilter filter, Pageable pageable);
 }
