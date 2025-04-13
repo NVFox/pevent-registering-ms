86928b86d379cfb03889f5584187d3c6a270aec5
Andrés Téllez <davidroa8@hotmail.com>
2025-04-13 00:54:44 -0500

    chore: remove migrated log repositories and updated entity

--- src/main/java/com/adt/registering/domain/entities/Log.java
+++ src/main/java/com/adt/registering/domain/entities/Log.java
@@ -1,29 +1,14 @@
 package com.adt.registering.domain.entities;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.CompoundIndex;
-import org.springframework.data.mongodb.core.index.IndexDirection;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.index.TextIndexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Document(collection = "logs")
-@CompoundIndex(def = "{'level': 1, 'timestamp': -1}")
 @Data
 public class Log {
-    @Id
     private String traceId;
-
-    @Indexed
     private Level level;
-
-    @TextIndexed
     private String message;
-
-    @Indexed(direction = IndexDirection.DESCENDING)
     private LocalDateTime timestamp = LocalDateTime.now();
 
     public static Log info(String message) {
