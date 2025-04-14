e230352c0d42ba086f0ef8f0be4fe58419350258
Andrés Téllez <davidroa8@hotmail.com>
2025-04-13 21:18:25 -0500

    chore: updated messaging config for reactive flow

--- src/main/java/com/adt/registering/infrastructure/adapters/messaging/RabbitMessagingPublisher.java
+++ src/main/java/com/adt/registering/infrastructure/adapters/messaging/RabbitMessagingPublisher.java
@@ -1,21 +1,46 @@
 package com.adt.registering.infrastructure.adapters.messaging;
 
 import com.adt.registering.application.providers.MessagingPublisher;
+import com.adt.registering.domain.entities.Log;
+import com.adt.registering.domain.services.LogService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.apache.commons.lang3.SerializationUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.rabbitmq.OutboundMessage;
+import reactor.rabbitmq.Sender;
 
 @Service
 @RequiredArgsConstructor
 public class RabbitMessagingPublisher implements MessagingPublisher {
-    private final RabbitTemplate rabbitTemplate;
+    private final Sender sender;
+    private final ObjectMapper objectMapper;
+
+    private final LogService logService;
 
     @Value("${messaging.notifications.topic-name}")
     private String topic;
 
     @Override
     public void publish(String channel, Object message) {
-        rabbitTemplate.convertAndSend(topic, channel, message);
+        try {
+            String json = objectMapper.writeValueAsString(message);
+            byte[] bytes = SerializationUtils.serialize(json);
+
+            Flux<OutboundMessage> outbound = Flux.just(new OutboundMessage(
+                    topic,
+                    channel,
+                    bytes
+            ));
+
+            sender.sendWithPublishConfirms(outbound)
+                    .doOnError(error ->
+                            logService.log(Log.error("Error publishing message: " + error.getMessage())))
+                    .subscribe();
+        } catch (Exception e) {
+            logService.log(Log.error("Error serializing message: " + e.getMessage()));
+        }
     }
 }
--- src/main/java/com/adt/registering/infrastructure/config/MessagingConfig.java
+++ src/main/java/com/adt/registering/infrastructure/config/MessagingConfig.java
@@ -1,64 +1,109 @@
 package com.adt.registering.infrastructure.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Connection;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.rabbitmq.RabbitFlux;
+import reactor.rabbitmq.Sender;
+import reactor.rabbitmq.SenderOptions;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Configuration
-@EnableRabbit
 public class MessagingConfig {
+    @Autowired
+    private Mono<Connection> connectionMono;
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    @Value("${messaging.notifications.topic-name}")
+    private String notificationTopic;
+
+    @Value("${messaging.notifications.queue-name}")
+    private String notificationQueue;
+
+    @Value("${messaging.notifications-logs.queue-name}")
+    private String logQueue;
+
+    @Value("${messaging.notifications.channel-name}")
+    private String notificationChannel;
+
+    @Value("${messaging.notifications-logs.channel-name}")
+    private String logChannel;
+
     @Bean
-    public Queue logQueue(@Value("${messaging.notifications-logs.queue-name}") String logQueue) {
-        return new Queue(logQueue, true);
+    public Mono<Connection> connectionMono(RabbitProperties rabbitProperties) {
+        com.rabbitmq.client.ConnectionFactory connectionFactory = new com.rabbitmq.client.ConnectionFactory();
+
+        connectionFactory.setHost(rabbitProperties.getHost());
+        connectionFactory.setPort(rabbitProperties.getPort());
+        connectionFactory.setUsername(rabbitProperties.getUsername());
+        connectionFactory.setPassword(rabbitProperties.getPassword());
+        connectionFactory.useNio();
+
+        return Mono.fromCallable(() -> connectionFactory
+                .newConnection("reactive-notifications-rabbitmq")).cache();
     }
 
     @Bean
-    public Queue notificationQueue(@Value("${messaging.notifications.queue-name}") String notificationQueue) {
-        return new Queue(notificationQueue, true);
+    public SenderOptions senderOptions(Mono<Connection> connectionMono) {
+        return new SenderOptions()
+                .connectionMono(connectionMono)
+                .resourceManagementScheduler(Schedulers.boundedElastic());
     }
 
     @Bean
-    public Exchange notificationsExchange(@Value("${messaging.notifications.topic-name}") String notificationTopic) {
+    public Sender sender(SenderOptions senderOptions) {
+        return RabbitFlux.createSender(senderOptions);
+    }
+
+    private Exchange notificationsExchange() {
         return new TopicExchange(notificationTopic);
     }
 
-    @Bean
-    public Binding logBinding(@Qualifier("logQueue") Queue logQueue,
-                              Exchange notificationsExchange,
-                              @Value("${messaging.notifications-logs.channel-name}") String channelName) {
-        return BindingBuilder.bind(logQueue)
-                .to(notificationsExchange)
-                .with(channelName)
+    private Queue notificationQueue() {
+        return new Queue(notificationQueue, true);
+    }
+
+    private Queue logQueue() {
+        return new Queue(logQueue, true);
+    }
+
+    private Binding notificationBinding() {
+        return BindingBuilder.bind(notificationQueue())
+                .to(notificationsExchange())
+                .with(notificationChannel)
                 .noargs();
     }
 
-    @Bean
-    public Binding notificationBinding(@Qualifier("notificationQueue") Queue notificationQueue,
-                                       Exchange notificationsExchange,
-                                       @Value("${messaging.notifications.channel-name}") String channelName) {
-        return BindingBuilder.bind(notificationQueue)
-                .to(notificationsExchange)
-                .with(channelName)
+    private Binding logBinding() {
+        return BindingBuilder.bind(logQueue())
+                .to(notificationsExchange())
+                .with(logChannel)
                 .noargs();
     }
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                         Jackson2JsonMessageConverter converter) {
-        RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(converter);
-        return template;
+    @PostConstruct
+    public void init() {
+        amqpAdmin.declareExchange(notificationsExchange());
+        amqpAdmin.declareQueue(notificationQueue());
+        amqpAdmin.declareQueue(logQueue());
+        amqpAdmin.declareBinding(notificationBinding());
+        amqpAdmin.declareBinding(logBinding());
     }
 
-    @Bean
-    public Jackson2JsonMessageConverter converter(ObjectMapper objectMapper) {
-        return new Jackson2JsonMessageConverter(objectMapper);
+    @PreDestroy
+    public void close() throws IOException {
+        Objects.requireNonNull(connectionMono.block()).close();
     }
 }
